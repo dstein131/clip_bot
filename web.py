6f38683c01252a8597d5fd6ba0790578804e706a
David Stein <dstein131@gmail.com>
2024-05-25 13:12:24 -0400

    1

--- web.py
+++ web.py
@@ -1,11 +1,12 @@
-import os
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "The bot is running!"
+def hello():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
