ccc7172115d702b030d0ff90f95c891f48071dc4
David Stein <dstein131@gmail.com>
2024-05-26 22:24:58 -0400

    33

--- bot.py
+++ bot.py
@@ -65,6 +65,7 @@ async def april(ctx):
             "https://i.imgur.com/BGvYhvE.jpeg",
             "https://i.imgur.com/3xqTqpq.gif",
             "https://i.imgur.com/NE9BsRN.jpeg",
+            "https://i.imgur.com/922Bazc.gif",
         ]
         selected_image = random.choice(april_images)
         print(f'Sending April image: {selected_image}')
