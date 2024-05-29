2f2af5e661866647db85908bc1221dce78960cb7
David Stein <dstein131@gmail.com>
2024-05-29 15:45:59 -0400

    456

--- fun_commands.py
+++ fun_commands.py
@@ -99,6 +99,7 @@ async def lew(ctx):
             "https://i.imgur.com/sCuFLi6.png",
             "https://i.imgur.com/vtzZgJs.jpeg",
             "https://i.imgur.com/TJDUGTX.jpeg",
+            "https://i.imgur.com/yLhEKnF.jpeg",
         ]
         selected_image = random.choice(lew_images)
         print(f'Sending Lew image: {selected_image}')
