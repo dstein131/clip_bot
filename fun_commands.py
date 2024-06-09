b27d979c82435d80f8ce3d1a436e2c3dbe0d4da7
David Stein <dstein131@gmail.com>
2024-06-09 08:47:21 -0400

    3

--- fun_commands.py
+++ fun_commands.py
@@ -195,6 +195,8 @@ async def moody(ctx):
         moody_images = [
             "https://i.imgur.com/NvXIKbQ.jpeg",
             "https://i.imgur.com/THWvdum.gif",
+            "https://i.imgur.com/2Bk5h46.gif",
+            "https://i.imgur.com/KFHsEdi.gif",
         ]
         selected_image = random.choice(moody_images)
         print(f'Sending Moody image: {selected_image}')
