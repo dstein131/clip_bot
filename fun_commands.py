41de6c4a79687094e31d2a59cd795fb002ec3038
David Stein <dstein131@gmail.com>
2024-06-10 08:01:51 -0400

    1

--- fun_commands.py
+++ fun_commands.py
@@ -232,6 +232,7 @@ async def aaron(ctx):
             "https://i.imgur.com/4oarokq.png",
             "https://i.imgur.com/a5zZvbp.png",
             "https://i.imgur.com/PdMKavl.png",
+            "https://i.imgur.com/pIB8xK0.gif",
            
         ]
         selected_image = random.choice(aaron_images)
